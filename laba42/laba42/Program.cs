48b8b8e7400bdb72f73afc0e51af631bb3307281
Родион Шароха <rodiok2001@mail.ru>
2020-12-04 00:23:39 +0500

    вычитание

--- laba42/laba42/Program.cs
+++ laba42/laba42/Program.cs
@@ -13,8 +13,13 @@ namespace laba42
                 Console.WriteLine("Число А");
                 double A = Convert.ToDouble(Console.ReadLine());
 
+                Console.WriteLine("Число B");
+                double B = Convert.ToDouble(Console.ReadLine());
 
-
+                double otv1 = A + B;
+                Console.WriteLine("Результат сложения" + Convert.ToString(otv1));
+                double otv2 = A - B;
+                Console.WriteLine("Результат вычитания" + Convert.ToString(otv2));
 
 
 
